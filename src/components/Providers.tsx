6832c8a370601df08a434ed81d5091c73da2506c
PSeshpavan <pseshpavan@gmail.com>
2025-10-02 17:49:16 +0530

    Trail 7-13 Again

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,16 +1,16 @@
 // // app/providers.tsx
- "use client";
+"use client";
 
- import { SessionProvider } from "next-auth/react";
+import { SessionProvider } from "next-auth/react";
 
- const BASE_PATH = "/1isto9-perfumery";
+const BASE_PATH = "/1isto9-perfumery";
 
- export default function Providers({ children }: { children: React.ReactNode }) {
-   console.log("SessionProvider basePath:", "/1isto9-perfumery/api/auth");
-   return (
-     <SessionProvider basePath={`${process.env.__NEXT_ROUTER_BASEPATH || ''}/api/auth`}>
-{/* <SessionProvider basePath="/1isto9-perfumery/api/auth"> */}
-       {children}
-     </SessionProvider>
-   );
- }
+export default function Providers({ children }: { children: React.ReactNode }) {
+  console.log("SessionProvider basePath:", "/1isto9-perfumery/api/auth");
+  return (
+    //  <SessionProvider basePath={`${process.env.__NEXT_ROUTER_BASEPATH || ''}/api/auth`}>
+    <SessionProvider basePath="/1isto9-perfumery/api/auth">
+      {children}
+    </SessionProvider>
+  );
+}
